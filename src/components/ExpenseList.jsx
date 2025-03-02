5ace1013affc0566e7a0963fad359a640fa338d2
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-03-03 02:39:23 +0530

    added some ui

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -8,8 +8,22 @@ export default function ExpenseList() {
     const {expenses} = useContext(ExpenseContext);
 
   return (
-    <div>
-      {expenses.length ===0 ? <h1 className='text-4xl text-green-600'>No Expenses</h1>:expenses.map((expense, index)=> (<ShowExpenses key={index} expense={expense}/>))}
-    </div>
+    <div className='bg-gray-100 min-h-56'>
+      {expenses.length ===0 ? (<h1 className='text-4xl text-green-600'>No Expenses</h1>):(
+      <table className=' bg-gray-300 w-5/6 ml-25 '>
+        
+      <tr>
+          <th className='border border-gray-500 px-4 py-4'>Expense Title</th>
+          <th className='border border-gray-500 px-4 py-4'>Amount (LKR)</th>
+          <th className='border border-gray-500 px-4 py-4'>Action</th>
+        </tr>
+        <tbody>
+        {expenses.map((expense, index)=> (<ShowExpenses key={index} expense={expense}/>))}
+      
+        
+        </tbody>
+      </table>
+)}
+</div>
   )
 }
--- src/components/ShowExpenses.jsx
+++ src/components/ShowExpenses.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
 
 export default function ShowExpenses({expense}) {
+
+ 
+
   return (
-    <div>
-        <div>{expense.title}</div>
-        <div>{expense.amount}</div>
-      
-    </div>
+    
+      <tr>
+            <td className='border border-gray-500 px-4 py-4'>{expense.title}</td>
+            <td className='border border-gray-500 px-4 py-4'>{expense.amount}</td>
+            <td className='border border-gray-400 px-4 py-4 flex justify-center gap-4'>
+              
+              <button className='border p-1 px-3 bg-green-500 hover:bg-green-600 rounded-xl'>Update</button>
+              <button className='border p-1 px-3 bg-red-500 hover:bg-red-600 rounded-xl'>Delete</button>
+             
+            </td>
+      </tr>
+   
   )
 }
